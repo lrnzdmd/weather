9d8f83af9a56f68a36dad805debf4f718a093f18
lrnzdmd <170991996+lrnzdmd@users.noreply.github.com>
2024-07-24 06:18:35 +0200

    successfully pulled data from the API! next up functions to get the input and make the user actually decide the city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-import "./style.css";
+
+
+
+
+const city = "roma";
+
+const cityData = fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&include=current&key=2BQ6Q849MVJELRWVMFGEMZRR6&contentType=json`, { mode: "cors" })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error("Network response error: " + response.statusText);
+        }
+        return response.json();
+    }).catch(error => {
+        console.error("Error fetching data from weather API: ",error);
+    });
+
+console.log(cityData);
